1332ea380abe77ca64064b25f2a3ee00212a0ef4
Blackbeing <johnjahi55@gmail.com>
2023-03-30 01:03:50 +0000

    fix Correction

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,7 +8,7 @@ $bash_script = @(END)
 #!/usr/bin/env bash
 
 # Get document root directory of server
-doc_root="$(grep -v '^#' /etc/nginx/sites-available/default | grep -w 'root' -m 1 | cut -d' ' -f2 | sed 's/;//')"
+doc_root="$(grep -v '^#' /etc/nginx/sites-enabled/default | grep -w 'root' -m 1 | cut -d' ' -f2 | sed 's/;//')"
 
 # create doc_root if not exists
 mkdir -p "$doc_root"
@@ -24,7 +24,7 @@ redirect_location="location /redirect_me {\n\t\treturn 301 https://google.com;\n
 
 # Get directive server_name _;, relace it with multiple string of redirect url
 
-sed -i "s|${base_location}|${base_location}\n\n\t${redirect_location}|" /etc/nginx/sites-available/default
+sed -i "s|${base_location}|${base_location}\n\n\t${redirect_location}|" /etc/nginx/sites-enabled/default
 
 # Reload nginx, (reload config without stopping)
 
@@ -34,12 +34,18 @@ service nginx restart
 END
 
 file { 'setup nginx':
-  path    => '/tmp/setup-nginx',
+  ensure  => 'file',
   mode    => '0755',
   content => $bash_script,
+  path    => '/tmp/setup-nginx',
+}
+
+exec { 'run nginx setup':
+  command => '/tmp/setup-nginx',
 }
 
 exec { 'test 301 redirect':
   command => 'curl localhost/redirect_me',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
+

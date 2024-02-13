bc4fc862ff1083935f0a69f15de0775717043aca
Blackbeing <johnjahi55@gmail.com>
2024-02-14 02:30:56 +0300

    feat Fetch top ten subreddit titles using API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This module queries reddit API using python"""
+
+import requests
+import json
+
+
+def top_ten(subreddit):
+    """Get top ten hot post of a subreddit """
+    base_url = "https://www.reddit.com"
+    url = "{}/r/{}/hot.json?limit=10".format(base_url, subreddit)
+    headers = {"User-Agent": "ALXAPIAdv/0.1"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        posts = response.json().get("data", {}).get("children", [])
+    except (requests.exceptions.RequestException,
+            json.decoder.JSONDecodeError):
+        return None
+    for post in posts:
+        title = post.get("data", {}).get("title", "")
+        print(title)

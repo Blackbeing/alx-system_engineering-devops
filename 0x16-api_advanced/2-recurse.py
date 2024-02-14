93d74846ca99378d0865a3848f0e56aa21abfe87
Blackbeing <johnjahi55@gmail.com>
2024-02-14 03:55:55 +0300

    feat Recursively fetch data from API

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""This module queries reddit API using python"""
+
+import requests
+import json
+
+
+base_url = "https://www.reddit.com"
+headers = {"User-Agent": "ALXAPIAdv/0.1"}
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    params = {"after": after}
+    url = "{}/r/{}/hot.json".format(base_url, subreddit)
+
+    try:
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
+        posts = response.json().get("data", {}).get("children", [])
+        hot_list.extend([post.get("data", {}).get("title", "")
+                        for post in posts])
+        after = response.json().get("data", {}).get("after")
+        if after:
+            return recurse(subreddit, hot_list, after)
+    except (requests.exceptions.RequestException,
+            json.decoder.JSONDecodeError):
+        return None
+
+    return hot_list
